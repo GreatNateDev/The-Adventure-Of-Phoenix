5420d3f5a0f230eafdef3c6a782517b8ad439811
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-04-26 11:33:42 -0700

    fix kb

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -9,23 +9,13 @@ var inv = false
 func _physics_process(delta):
 		velocity = (player.get_global_position() - position).normalized() * speed * delta
 		move_and_collide(velocity)
-		for body in $player_intersector.get_overlapping_bodies():
-			if body.name == "Phoenix":
-				pass
-				#fix kb stuffs
 func _on_player_intersector_body_entered(body):
 	if body.name == "Phoenix":
 		player.hp -= damage
 		knockback("player")
-
-
 func _on_player_intersector_area_entered(area:Area2D):
 	if area.name == "sword" and inv == false:
 		hits -= player.power
-		print(hits)
-		print(player.power)
-		print("hit")
-		#knockback("self")
 		inv = true
 		$Timer.start()
 		if hits <= 0:
@@ -35,10 +25,5 @@ func knockback(who):
 			var kbdir = (velocity - Vector2(0,0)).normalized() *kbpwr
 			player.velocity = kbdir
 			player.move_and_slide()
-	#elif who == "self":
-		#var kbdir = (self.velocity + player.last_mov).normalized() * kbpwr
-		#self.velocity = kbdir
-		#self.move_and_slide()
-		#kb Code
 func _on_timer_timeout():
 	inv = false
