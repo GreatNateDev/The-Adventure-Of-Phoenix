26854824006f164770518fee33e86f045958f64a
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-04-27 14:27:32 -0700

    ultra commit

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -5,10 +5,18 @@ var hits = 4
 var kbpwr = 2000
 var take_kb = false
 var inv = false
+var timed = false
+var mov = true
 @onready var player = get_parent().get_node("Phoenix")
 func _physics_process(delta):
-		velocity = (player.get_global_position() - position).normalized() * speed * delta
-		move_and_collide(velocity)
+	if mov == true:
+			velocity = (player.get_global_position() - position).normalized() * speed * delta
+			move_and_collide(velocity)
+			print($Timer.time_left)
+	if $Timer.time_left <= 0.8 and timed == false and not $Timer.time_left <= 0:
+		timed = true
+		self.hide()
+		print("ATTEMPT")
 func _on_player_intersector_body_entered(body):
 	if body.name == "Phoenix":
 		player.hp -= damage
@@ -18,6 +26,7 @@ func _on_player_intersector_area_entered(area:Area2D):
 		hits -= player.power
 		inv = true
 		$Timer.start()
+		mov = false
 		if hits <= 0:
 			self.queue_free()
 func knockback(who):
@@ -27,3 +36,6 @@ func knockback(who):
 			player.move_and_slide()
 func _on_timer_timeout():
 	inv = false
+	timed = false
+	mov = true
+	self.show()
