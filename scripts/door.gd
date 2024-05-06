26a4585dfb57d3d7a555e29a2db737e7a7af51da
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-05-06 15:33:01 -0700

    fix door besides going back out to hyrule

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,18 +1,23 @@
 extends Area2D
 @export var area = "null"
-
+var phoenix = null
 func _on_body_entered(body:Node2D):
 	if body.name == "Phoenix":
+		phoenix = body
 		if area == "sword_cave":
 			global.entering_building = true
 			$trans_time.start()
-		elif area == "sub_1":
-			pass
+		elif area == "b_sword_cave":
+			global.entering_building = true
+			$trans_time.start()
 
 
 func _on_trans_time_timeout():
 	if area == "sword_cave":
 		get_tree().change_scene_to_file("res://scenes/sword_cave.tscn")
 		global.just_entered_building = true
-	elif area == "sub_1":
-		pass
+	elif area == "b_sword_cave":
+		get_tree().change_scene_to_file("res://scenes/hyrule.tscn")
+		global.just_entered_building = true
+		
+		
