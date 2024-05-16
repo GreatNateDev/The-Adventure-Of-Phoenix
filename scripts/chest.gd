eaceb387f2d60e79d3a889c4bafb817ca234c6fb
Nate <99703235+GreatNateDev@users.noreply.github.com>
2024-05-15 19:14:56 -0700

    get all the ruppees working for the chest + change chest z order and add a cool coding plugin

--- scripts/chest.gd
+++ scripts/chest.gd
@@ -1,19 +1,33 @@
 extends Sprite2D
-var green_ruppee = preload("res://scenes/green_ruppee.tscn")
 @export var item : String
+var item_for_func : PackedScene
 var player = null
 var opened = true
+var spawned = false
+func _physics_process(delta):
+	if item == "green_ruppee":
+		item_for_func = preload("res://scenes/green_ruppee.tscn")
+	elif item == "blue_ruppee":
+		item_for_func = preload("res://scenes/blue_ruppee.tscn")
+	elif item == "red_ruppee":
+		item_for_func = preload("res://scenes/red_ruppee.tscn")
+	elif item == "purple_ruppee":
+		item_for_func = preload("res://scenes/purple_ruppee.tscn")
+	elif item == "gold_ruppee":
+		item_for_func = preload("res://scenes/gold_ruppee.tscn")
 func on_player_entered(body):
 	if body.name == "Phoenix":
 		player = body
 func _process(_delta):
-	if player != null:
-		if item == "green_ruppee":
+	if spawned == false:
+		if player != null:
 			if Input.is_action_just_pressed("sword"):
-				$AnimationPlayer.play("open")
-				var reward = green_ruppee.instantiate()
-				get_parent().add_child(reward)
-				reward.position = self.position
-				reward.position.y -= 5
-				opened = true
-				
+				print(item_for_func)
+				run(item_for_func)
+func run(itom : PackedScene):
+	spawned = true
+	$AnimationPlayer.play("open")
+	var reward = itom.instantiate()
+	get_parent().add_child(reward)
+	reward.position = self.position
+	reward.position.y -= 5
